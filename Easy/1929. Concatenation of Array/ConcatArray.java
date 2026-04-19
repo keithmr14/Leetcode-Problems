89d7ec32a513ca7e870b96fdfa3c02229b4e520a
Keith Marlou Aldueza <keithmr14@gmail.com>
2026-04-20 03:55:01 +0800

    Create ConcatArray.java

---
+++ Easy/1929. Concatenation of Array/ConcatArray.java
@@ -0,0 +1,30 @@
+    public static int[] getConcatenation(int[] nums) {
+
+        int n = nums.length;
+        int[] ans = new int[n * 2];
+
+        for(int i = 0; i < n; i++) {
+
+            int insert = nums[i];
+            ans[i] = insert;
+            ans[i + n] = insert;
+        }
+
+        return ans;
+    }
+
+    static void main(String[] args) {
+
+        System.out.println("1929. Concatenation of Array\n");
+
+        // example 1
+        int[] a1 = {1, 2, 1};
+        System.out.println("ORG: " + Arrays.toString(a1));
+        System.out.println("NEW: " + Arrays.toString(getConcatenation(a1)) + "\n");
+
+        // example 2
+        int[] a2 = {1, 3, 2, 1};
+        System.out.println("ORG: " + Arrays.toString(a2));
+        System.out.println("NEW: " + Arrays.toString(getConcatenation(a2)) + "\n");
+    }
+}
