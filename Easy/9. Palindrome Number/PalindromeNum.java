0f4f92fd6fc08a4d72fd08aaca39aaf215006f88
Keith Marlou Aldueza <keithmr14@gmail.com>
2026-05-27 00:30:22 +0800

    Updated PalindromeNum.java

--- Easy/9. Palindrome Number/PalindromeNum.java
+++ Easy/9. Palindrome Number/PalindromeNum.java
@@ -1,35 +1,35 @@
 public class PalindromeNum {
 
     public static boolean isPalindrome(int x) {
+
         // turn the number to String so we can use .charAt
         String num = String.valueOf(x);
+
         int i = 0; // first char pointer
         int j = num.length() - 1; // last char pointer
-        // if pointing at the same character or moved past each other,
-        // there's no need for comparison
-        while(i < j) {
-            // if both ends are symmetric move pointer closer together
-            if(num.charAt(i) == num.charAt(j)) {
-                i++;
-                j--;
-            }
+
+        while(i < j) { // while not pointing at the same char or moved past each other
+
+            // if both ends have equal value, move pointer closer to each other
+            if(num.charAt(i) == num.charAt(j)) { i++; j--; }
             else return false;
         }
-        return true;
+
+        return true; // if no mismatched number, return true
     }
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
 
-        System.out.println("9. Palindrome Number\n");
+        System.out.println("9. Palindrome Number");
 
         // example 1
         int n1 = 11;
-        System.out.println(n1);
-        System.out.println("Is Palindrome? " + isPalindrome(n1) + "\n");
+        System.out.println("\nNumber: " + n1);
+        System.out.println("Is Palindrome? " + isPalindrome(n1));
 
         // example 2
         int n2 = -121;
-        System.out.println(n2);
-        System.out.println("Is Palindrome? " + isPalindrome(n2) + "\n");
+        System.out.println("\nNumber: " + n2);
+        System.out.println("Is Palindrome? " + isPalindrome(n2));
     }
 }
