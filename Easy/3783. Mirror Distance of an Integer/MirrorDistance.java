f2533fd04762cd52c30d595888c31566dd467f93
Keith Marlou Aldueza <keithmr14@gmail.com>
2026-05-28 00:01:22 +0800

    Update MirrorDistance.java

--- Easy/3783. Mirror Distance of an Integer/MirrorDistance.java
+++ Easy/3783. Mirror Distance of an Integer/MirrorDistance.java
@@ -1,22 +1,36 @@
-public class MirrorDistance {
+public class MirrorDist {
 
     public static int mirrorDistance(int n) {
 
-        String reversedStr = new StringBuilder(String.valueOf(n)).reverse().toString();
-        int reversedNum = Integer.parseInt(reversedStr);
+        int num = n;
+        // last digit * 10^pow will be the last digit in the first digit place
+        int pow = String.valueOf(n).length() - 1;
 
-        System.out.println("||" + n + " - " + reversedNum + "||");
-        return Math.abs(n - reversedNum);
+        while(num != 0) {
+
+            int lastDigit = num % 10;
+            int tenthPlace = (int) Math.pow(10, pow);
+            // subtract the last digit in the first digit place to n
+            n -= lastDigit * tenthPlace;
+            num /= 10; // this will change the last digit we're accessing
+            pow--; // reduce the tenth place
+        }
+
+        return Math.abs(n); // return the absolute distance
     }
 
     public static void main(String[] args) {
 
-        System.out.println("3783. Mirror Distance of an Integer\n");
+        System.out.println("3783. Mirror Distance of an Integer");
 
         // example 1
-        System.out.println("Mirror Distance: " + mirrorDistance(25) + "\n");
+        int n1 = 25;
+        System.out.println("\nInteger: " + n1);
+        System.out.println("Mirror Distance: " + mirrorDistance(n1));
 
         // example 2
-        System.out.println("Mirror Distance: " + mirrorDistance(10) + "\n");
+        int n2 = 10;
+        System.out.println("\nInteger: " + n2);
+        System.out.println("Mirror Distance: " + mirrorDistance(n2));
     }
 }
