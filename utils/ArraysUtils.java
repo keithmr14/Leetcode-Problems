93b11cefd947038066373bb1dee60a1ea49ed731
Keith Marlou Aldueza <keithmr14@gmail.com>
2026-06-08 16:19:20 +0800

    Create ArraysUtils.java

---
+++ utils/ArraysUtils.java
@@ -0,0 +1,22 @@
+package utils;
+
+import java.util.Arrays;
+
+public class ArrayUtils {
+
+    public static void print2dIntOneLine(int[][] arrays) {
+        int n = arrays.length;
+        for(int i = 0; i < n; i++) {
+            System.out.print(Arrays.toString(arrays[i]));
+            if(i != n - 1) System.out.print(", ");
+        }
+    }
+
+    public static void print2dIntMultiLine(int[][] arrays) {
+        int n = arrays.length;
+        for(int i = 0; i < n; i++) {
+            System.out.print(Arrays.toString(arrays[i]));
+            if(i != n - 1) System.out.print("\n");
+        }
+    }
+}
