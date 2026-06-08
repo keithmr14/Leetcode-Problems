4307bc823b26b793be7db60a60b8a2d714ceb11c
Keith Marlou Aldueza <keithmr14@gmail.com>
2026-06-08 16:24:22 +0800

    Update ListUtils.java

--- utils/ListUtils.java
+++ utils/ListUtils.java
@@ -2,14 +2,9 @@ package utils;
 
 public class ListUtils {
 
-    // private constructor prevents instantiation
-    private ListUtils() {
-        throw new UnsupportedOperationException("Utility class cannot be instantiated.");
-    }
-
     public static ListNode buildList(int[] arr) {
-        if (arr == null || arr.length == 0) return null;
 
+        if (arr == null || arr.length == 0) return null;
         ListNode dummy = new ListNode();
         ListNode curr = dummy;
 
@@ -17,11 +12,11 @@ public class ListUtils {
             curr.next = new ListNode(val);
             curr = curr.next;
         }
-
         return dummy.next;
     }
 
     public static void printList(ListNode head) {
+
         System.out.print("[");
 
         while (head != null) {
@@ -29,7 +24,6 @@ public class ListUtils {
             if (head.next != null) System.out.print(", ");
             head = head.next;
         }
-
         System.out.print("]\n");
     }
 }
