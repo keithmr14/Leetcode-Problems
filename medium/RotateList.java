ad79ab985b02d35d29a5c21ef3d70b1ce2d10e4b
Keith Marlou Aldueza <keithmr14@gmail.com>
2026-06-09 03:03:05 +0800

    Restructure

--- medium/RotateList.java
+++ medium/RotateList.java
@@ -8,19 +8,20 @@ public class RotateList {
 
     public static ListNode rotateRight(ListNode head, int k) {
 
-        if (head == null || head.next == null || k == 0) { return head; }
-        
+        if(k < 0) throw new IllegalArgumentException("k is invalid");
+
+        if(head == null || head.next == null || k == 0) return head;
+
         ListNode curr = head;
         int n = 1;
-        
-        // go to the last node
+
         while(curr.next != null) { curr = curr.next; n++; }
 
         curr.next = head; // connect both ends
         k = k % n;
-        int cut = n - k; // node before the new head
-        
-        for(int i = 1; i <= cut; i++) { curr = curr.next; }
+        int newHeadAt = n - k - 1; // 1 node before the new head index (before rotation, 0-indexed)
+
+        for(int i = 0; i <= newHeadAt; i++) { curr = curr.next; }
 
         head = curr.next;
         curr.next = null;
@@ -31,7 +32,7 @@ public class RotateList {
     public static void main(String[] args) {
 
         System.out.println("61. Rotate List");
-        
+
         // example 1
         int[] a1 = {1, 2, 3, 4, 5};
         int k1 = 2;
@@ -39,7 +40,7 @@ public class RotateList {
         System.out.println("\nk = " + k1 + "    " +  Arrays.toString(a1));
         System.out.print("Rotated: ");
         printList(rotateRight(h1, k1));
-        
+
         // example 2
         int[] a2 = {0, 1, 2};
         int k2 = 4;
