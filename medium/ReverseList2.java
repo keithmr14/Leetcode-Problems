a70c83f88b7dee0d859be549e53936effde8b543
Keith Marlou Aldueza <keithmr14@gmail.com>
2026-06-08 16:39:11 +0800

    Update ReverseList2.java

--- medium/ReverseList2.java
+++ medium/ReverseList2.java
@@ -7,14 +7,16 @@ import java.util.Arrays;
 public class ReverseList2 {
 
     public static ListNode reverseBetween(ListNode head, int left, int right) {
-        
+
         ListNode dummy = new ListNode();
         dummy.next = head;
         ListNode curr = head;
         ListNode nodeInRange = null; // pointer for left, right, and in between
         ListNode beforeleft = dummy; // one node before left when left = 1 or update this
+        
         // stop on or before left's node
         for(int i = 1; i <= left; i++) {
+            
             if(i + 1 == left) {
                 beforeleft = curr;
                 nodeInRange = curr.next;
@@ -25,16 +27,20 @@ public class ReverseList2 {
         }
         ListNode tail = nodeInRange; // tail of the reverse range
         ListNode prev = tail; // when reversing node, point those node to prev
+
         // tail doesn't have a node to point yet, so start reversing at the next of left's node
         assert nodeInRange != null: "left node not found";
         nodeInRange = nodeInRange.next;
         left++;
+        
         while(left <= right) {
+            
             if(left == right) { // finalize reversing the range
                 ListNode afterRight = nodeInRange.next;
                 nodeInRange.next = prev;
                 beforeleft.next = nodeInRange;
                 tail.next = afterRight;
+                
             } else { // reverse in between nodes
                 ListNode next = nodeInRange.next;
                 nodeInRange.next = prev;
@@ -49,6 +55,7 @@ public class ReverseList2 {
     public static void main(String[] args) {
 
         System.out.println("92. Reverse Linked List II");
+        
         // example 1
         int[] a1 = {1, 2, 3, 4, 5};
         int l1 = 2;
@@ -58,6 +65,7 @@ public class ReverseList2 {
         System.out.println("Left: " + l1 + " Right: " + r1);
         System.out.print("Reversed: ");
         printList(reverseBetween(h1, l1, r1));
+        
         // example 2
         int[] a2 = {5};
         int l2 = 1;
