85a7376b2360242005bc3ae6fc331dba27a5829b
Keith Marlou Aldueza <keithmr14@gmail.com>
2026-06-08 16:40:35 +0800

    Update RotateList.java

--- medium/RotateList.java
+++ medium/RotateList.java
@@ -9,14 +9,17 @@ public class RotateList {
     public static ListNode rotateRight(ListNode head, int k) {
 
         if (head == null || head.next == null || k == 0) { return head; }
+        
         ListNode curr = head;
         int n = 1;
+        
         // go to the last node
         while(curr.next != null) { curr = curr.next; n++; }
 
         curr.next = head; // connect both ends
         k = k % n;
         int cut = n - k; // node before the new head
+        
         for(int i = 1; i <= cut; i++) { curr = curr.next; }
 
         head = curr.next;
@@ -28,6 +31,7 @@ public class RotateList {
     public static void main(String[] args) {
 
         System.out.println("61. Rotate List");
+        
         // example 1
         int[] a1 = {1, 2, 3, 4, 5};
         int k1 = 2;
@@ -35,6 +39,7 @@ public class RotateList {
         System.out.println("\nk = " + k1 + "    " +  Arrays.toString(a1));
         System.out.print("Rotated: ");
         printList(rotateRight(h1, k1));
+        
         // example 2
         int[] a2 = {0, 1, 2};
         int k2 = 4;
