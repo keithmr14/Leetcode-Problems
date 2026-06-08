d8f3c01a052588559011f963911f5001e0db173c
Keith Marlou Aldueza <keithmr14@gmail.com>
2026-06-08 16:23:33 +0800

    Update ArraysUtils.java

--- utils/ArraysUtils.java
+++ utils/ArraysUtils.java
@@ -5,7 +5,9 @@ import java.util.Arrays;
 public class ArrayUtils {
 
     public static void print2dIntOneLine(int[][] arrays) {
+        
         int n = arrays.length;
+        
         for(int i = 0; i < n; i++) {
             System.out.print(Arrays.toString(arrays[i]));
             if(i != n - 1) System.out.print(", ");
@@ -13,7 +15,9 @@ public class ArrayUtils {
     }
 
     public static void print2dIntMultiLine(int[][] arrays) {
+        
         int n = arrays.length;
+        
         for(int i = 0; i < n; i++) {
             System.out.print(Arrays.toString(arrays[i]));
             if(i != n - 1) System.out.print("\n");
