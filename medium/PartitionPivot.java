c72b849a909e9d9fbc4062081adb32faa800a023
Keith Marlou Aldueza <keithmr14@gmail.com>
2026-06-13 14:41:09 +0800

    Update PartitionPivot.java

--- medium/PartitionPivot.java
+++ medium/PartitionPivot.java
@@ -8,11 +8,11 @@ public class PartitionPivot {
     public static int[] pivotArray(int[] nums, int pivot) {
 
         ListNode dummy = new ListNode();
-        // starts creating nodes at the start
+        // starts creating left of pivot and pivot at the start
         ListNode maxLowerRange = dummy;
         ListNode pivotEnd = new ListNode();
         boolean foundPivot = false;
-        // starts creating nodes separately
+        // starts creating right of pivot separately
         ListNode minHigherRange = null;
         ListNode lastNode = new ListNode();
         boolean foundHigher = false;
@@ -20,16 +20,19 @@ public class PartitionPivot {
         for(int num : nums) {
 
             if(num == pivot) {
+
                 ListNode next = maxLowerRange.next;
                 ListNode addPivot = new ListNode(pivot);
                 maxLowerRange.next = addPivot;
                 addPivot.next = next;
+
                 if(!foundPivot) {
                     pivotEnd = addPivot;
                     foundPivot = true;
                 }
             }
             else if(num > pivot) {
+
                 if(!foundHigher) {
                     minHigherRange = new ListNode(num);
                     lastNode = minHigherRange;
@@ -39,14 +42,15 @@ public class PartitionPivot {
                     lastNode = lastNode.next;
                 }
             }
-            else if(num < pivot) {
+            else { // num < pivot
                 ListNode next = maxLowerRange.next;
                 maxLowerRange.next = new ListNode(num);
                 maxLowerRange = maxLowerRange.next;
                 maxLowerRange.next = next;
             }
         }
-        pivotEnd.next = minHigherRange;
+        if(!foundPivot) maxLowerRange.next = minHigherRange;
+        else pivotEnd.next = minHigherRange;
 
         for (int i = 0; dummy.next != null; i++) {
             nums[i] = dummy.next.val;
@@ -66,11 +70,18 @@ public class PartitionPivot {
         System.out.println("Pivot: " + p1);
         System.out.println("Partition: " + Arrays.toString(pivotArray(a1, p1)));
 
-        // example 1
+        // example 2
         int[] a2 = {4, 0, 4, 5, -11};
         int p2 = 5;
         System.out.println("\nArray: " + Arrays.toString(a2));
         System.out.println("Pivot: " + p2);
         System.out.println("Partition: " + Arrays.toString(pivotArray(a2, p2)));
+
+        // example 3
+        int[] a3 = {};
+        int p3 = -1;
+        System.out.println("\nArray: " + Arrays.toString(a3));
+        System.out.println("Pivot: " + p3);
+        System.out.println("Partition: " + Arrays.toString(pivotArray(a3, p3)));
     }
 }
