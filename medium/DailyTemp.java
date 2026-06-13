cc84ca4e6f0dedf29f1ecd9261b9eb36b1a2d2e3
Keith Marlou Aldueza <keithmr14@gmail.com>
2026-06-13 17:17:42 +0800

    Update DailyTemp.java

--- medium/DailyTemp.java
+++ medium/DailyTemp.java
@@ -10,36 +10,34 @@ public class DailyTemp {
         Stack<Integer> stack = new Stack<>(); // stack of waiting temperature indices
 
         for(int i = 0; i < temperatures.length; i++) {
-            
+
             int temp = temperatures[i];
-            
+
             while(!stack.empty()) {
                 int topI = stack.peek();
-                
-                // if temp is not > than peek, it is not > the rest of the stack
+                // if temp is not greater than peek, it is not greater than the rest of the stack
                 if(temp <= temperatures[topI]) break;
-                
-                // calculate wait time, pop, & see if temp is still > the rest of the stack
+
                 temperatures[topI] = i - topI;
                 stack.pop();
             }
-            stack.push(i); // when all/no comparison done
+            stack.push(i);
         }
-        // set wait time to 0 for all remaining index in the stack
+
         while(!stack.empty()) temperatures[stack.pop()] = 0;
 
-        return temperatures; // updated as wait time for all temperatures
+        return temperatures; // reused space fo wait time of all temperatures
     }
 
     public static void main(String[] args) {
 
         System.out.println("739. Daily Temperatures");
-        
+
         // example 1
         int[] a1 = {2, 1, 2, 3};
         System.out.println("\nTemperature: " + Arrays.toString(a1));
         System.out.println("Wait Time: " + Arrays.toString(dailyTemperatures(a1)));
-        
+
         // example 2
         int[] a2 = {30, 40, 50, 60};
         System.out.println("\nTemperature: " + Arrays.toString(a2));
