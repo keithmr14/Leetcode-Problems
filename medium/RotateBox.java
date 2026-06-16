20423941ab29f86489bfd9a4e50048aabe437ad7
Keith Marlou Aldueza <keithmr14@gmail.com>
2026-06-17 04:27:21 +0800

    Restructure

---
+++ medium/RotateBox.java
@@ -0,0 +1,77 @@
+package medium;
+
+import static utils.ArrayUtils.*;
+
+public class RotateBox {
+
+    public static char[][] rotateTheBox(char[][] boxGrid) {
+
+        if(boxGrid.length == 0) throw new IllegalArgumentException("matrix is null");
+
+        int rows = boxGrid.length;
+        int cols = boxGrid[0].length;
+
+        // move falling characters to the right first before rotating
+        for(int i = 0; i < rows; i++) {
+
+            if(boxGrid[i].length != cols) throw new IllegalArgumentException("the matrix have different row lengths");
+            if(boxGrid[i].length == 0) throw new IllegalArgumentException("a row was empty: width of 0");
+
+            int fallAt = -1;
+
+            for(int j = cols - 1; j >= 0; j--) {
+
+                if(boxGrid[i][j] == '.') {
+                    if(fallAt == -1) fallAt = j;
+                }
+                else if(boxGrid[i][j] == '*') {
+                    fallAt = -1;
+                }
+                else if(boxGrid[i][j] == '#') {
+                    if(fallAt != -1) {
+                        boxGrid[i][j] = '.';
+                        boxGrid[i][fallAt] = '#';
+                        fallAt--;
+                    }
+                }
+                else throw new IllegalArgumentException("invalid character found");
+            }
+        }
+
+        char[][] rotated = new char[cols][rows];
+
+        for(int i = 0; i < cols; i++)
+
+            for(int j = 0; j < rows; j++)
+
+                rotated[i][j] = boxGrid[rows - 1 - j][i];
+
+        return rotated;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("1861. Rotating the Box");
+
+        // example 1
+        char[][] a1 = {{'#', '.', '.'}, {'#', '*', '.'}};
+        System.out.println("\nOriginal:");
+        print2dPrimMultiLine(a1);
+        System.out.println("Rotated: ");
+        print2dPrimMultiLine(rotateTheBox(a1));
+
+        // example 2
+        char[][] a2 = {{'#', '.', '*', '.'}, {'#', '#', '*', '.'}};
+        System.out.println("\nOriginal:");
+        print2dPrimMultiLine(a2);
+        System.out.println("Rotated: ");
+        print2dPrimMultiLine(rotateTheBox(a2));
+
+        // example 3
+        char[][] a3 = {{'#', '.', '.'}};
+        System.out.println("\nOriginal:");
+        print2dPrimMultiLine(a3);
+        System.out.println("Rotated: ");
+        print2dPrimMultiLine(rotateTheBox(a3));
+    }
+}
