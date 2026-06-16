3505ccc6fe85fee893dfbb6eecb4414c4de8c2ca
Keith Marlou Aldueza <keithmr14@gmail.com>
2026-06-16 16:59:22 +0800

    Update ArraysUtils.java

--- utils/ArraysUtils.java
+++ utils/ArraysUtils.java
@@ -5,22 +5,53 @@ import java.util.Arrays;
 public class ArrayUtils {
 
     public static void print2dIntOneLine(int[][] arrays) {
-        
+
         int n = arrays.length;
-        
+
         for(int i = 0; i < n; i++) {
+            
             System.out.print(Arrays.toString(arrays[i]));
             if(i != n - 1) System.out.print(", ");
         }
     }
 
     public static void print2dIntMultiLine(int[][] arrays) {
-        
+
         int n = arrays.length;
-        
+
         for(int i = 0; i < n; i++) {
+            
             System.out.print(Arrays.toString(arrays[i]));
             if(i != n - 1) System.out.print("\n");
         }
+    }
+
+    public static boolean checkNonIncreasing2Array(int[] a1, int[] a2) {
+
+        boolean first = true;
+        int lastNum = -1;
+
+        for(int num : a1) {
+            if(!first) {
+                if(num > lastNum) return false;
+            }
+            else first = false;
+
+            lastNum = num;
+        }
+
+        first = true;
+        lastNum = -1;
+
+        for(int num : a2) {
+            if(!first) {
+                if(num > lastNum) return false;
+            }
+            else first = false;
+
+            lastNum = num;
+        }
+
+        return true;
     }
 }
