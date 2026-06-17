4009c16f82df02591444e13bab3bd61ca16b478c
Keith Marlou Aldueza <keithmr14@gmail.com>
2026-06-18 05:50:24 +0800

    Restructure

---
+++ medium/RotatedDigits.java
@@ -0,0 +1,65 @@
+package medium;
+
+public class RotatedDigits {
+
+    private static int[] memo = new int[10];
+    private static int maxN = 0;
+
+    public static int rotatedDigits(int n) {
+
+        if(n < 1) throw new IllegalArgumentException("n must be >= 1");
+
+        if(n >= memo.length) {
+
+            int newSize = n + 1;
+            int[] newMemo = new int[newSize];
+            System.arraycopy(memo, 0, newMemo, 0, memo.length);
+            memo = newMemo;
+        }
+
+        if (n > maxN) {
+            int good = memo[maxN]; // 1-indexed
+
+            for (int currNum = maxN + 1; currNum <= n; currNum++) {
+
+                if (isGoodNumber(currNum)) good++;
+
+                memo[currNum] = good;
+            }
+            maxN = n;
+        }
+
+        return memo[n];
+    }
+
+    private static boolean isGoodNumber(int num) {
+
+        boolean valid = false;
+
+        while (num >= 1) {
+            int lastDig = num % 10;
+
+            if (lastDig == 2 || lastDig == 5 || lastDig == 6 || lastDig == 9) valid = true;
+
+            else if (lastDig == 3 || lastDig == 4 || lastDig == 7) return false;
+
+            num /= 10; // 1 and 0 are valid but doesn't change so we need to check for more digit
+        }
+        return valid;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("788. Rotated Digits");
+
+        // example 1
+        int n1 = 857;
+        System.out.println("\nRange: [1, " + n1 + "]");
+        System.out.println("Good Numbers: " + rotatedDigits(n1));
+
+        // example 2
+        int n2 = 10;
+        System.out.println("\nRange: [1, " + n2 + "]");
+        System.out.println("Good Numbers: " + rotatedDigits(n2));
+    }
+}
