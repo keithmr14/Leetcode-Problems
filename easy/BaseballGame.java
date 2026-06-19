69acc0fefb390e036dd55eda04c2554d1c95fbdb
Keith Marlou Aldueza <keithmr14@gmail.com>
2026-06-20 02:59:12 +0800

    Update BaseballGame.java

--- easy/BaseballGame.java
+++ easy/BaseballGame.java
@@ -1,7 +1,6 @@
 package easy;
 
-import java.util.Arrays;
-import java.util.Stack;
+import java.util.*;
 
 public class BaseballGame {
 
@@ -13,28 +12,38 @@ public class BaseballGame {
         for (String op : operations) {
             switch (op) {
                 case "+": // record the sum of the last two num
-                    int top = nums.pop(); 
-                    int sum = nums.peek() + top;
-                    nums.push(top);
-                    nums.push(sum);
+                    if(nums.size() < 2) throw new IllegalArgumentException("there's not enough scores to add prior to '+'");
+                    int top = nums.pop();
+                    try {
+                        int sum = Math.addExact(nums.peek(), top);
+                        nums.push(top);
+                        nums.push(sum);
+                    }
+                    catch(ArithmeticException e) {
+                        throw new IllegalArgumentException(top + " + " + nums.peek() + " resulted in integer overflow"); }
                     break;
 
                 case "D": // record 2 times the last num
-                    nums.push(nums.peek() * 2);
+                    if(nums.isEmpty()) throw new IllegalArgumentException("there's not enough scores to multiply prior to 'D'");
+                    try {
+                        int product = Math.multiplyExact(nums.peek(), 2);
+                        nums.push(product);
+                    }
+                    catch(ArithmeticException e) {
+                        throw new IllegalArgumentException(nums.peek() + " * 2 resulted in an integer overflow"); }
                     break;
 
                 case "C": // invalidate last num
+                    if(nums.isEmpty()) throw new IllegalArgumentException("there's not enough scores to invalidate prior to 'C'");
                     nums.pop();
                     break;
 
                 default: // record any number
-
-                    try { Integer.parseInt(op); }
-
+                    try {
+                        nums.push(Integer.parseInt(op));
+                    }
                     catch(NumberFormatException e) {
                         throw new IllegalArgumentException("operation \"" + op + "\" is invalid"); }
-
-                    nums.push(Integer.parseInt(op));
             }
         }
         // add all the scores
