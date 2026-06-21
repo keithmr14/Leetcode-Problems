1bbe1541bffc8bdfd909b0ea16bd7ec80b3735f4
Keith Marlou Aldueza <keithmr14@gmail.com>
2026-06-21 11:45:44 +0800

    Update LeftRightSumDiff.java

--- easy/LeftRightSumDiff.java
+++ easy/LeftRightSumDiff.java
@@ -8,33 +8,42 @@ public class LeftRightSumDiff {
 
         int n = nums.length;
         int[] diff = new int[n];
-        
+
         int left = 0;
         // left traverse to record left sum
         for(int i = 0; i < n; i++) {
             diff[i] = left;
-            left += nums[i];
+            try {
+                left = Math.addExact(left, nums[i]);
+            }
+            catch(ArithmeticException e) {
+                throw new IllegalArgumentException("sum of the elements causes an integer overflow"); }
         }
-        
+
         int right = 0;
         // right traverse and subtract right to left sum (then abs)
         for(int i = n - 1; i >= 0; i--) {
-            diff[i] = Math.abs(diff[i] - right);
+            try {
+                diff[i] = Math.absExact(Math.subtractExact(diff[i], right));
+            }
+            catch(ArithmeticException e) {
+                throw new IllegalArgumentException("difference of right and left sum causes an integer overflow"); }
+
             right += nums[i];
         }
-        
+
         return diff;
     }
 
     public static void main(String[] args) {
 
         System.out.println("2574. Left and Right Sum Differences");
-        
+
         // example 1
         int[] a1 = {10, 4, 8, 3};
         System.out.println("\nArray: " + Arrays.toString(a1));
         System.out.println("Differences: " + Arrays.toString(leftRightDifference(a1)));
-        
+
         // example 2
         int[] a2 = {1, 1, 1, 1};
         System.out.println("\nArray: " + Arrays.toString(a2));
