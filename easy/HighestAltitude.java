3c111087bb32546d4ccbf8f05ced004cc85be467
Keith Marlou Aldueza <keithmr14@gmail.com>
2026-08-19 08:52:24 +0800

    Update HighestAltitude.java

--- easy/HighestAltitude.java
+++ easy/HighestAltitude.java
@@ -9,12 +9,15 @@ public class HighestAltitude {
         int maxAlt = 0;
         int currAlt = 0;
 
-        for(int gain : gains) {
+        for(int i = 0; i < gains.length; i++) {
+
+            int gain = gains[i];
             try {
                 currAlt = Math.addExact(currAlt, gain);
             }
             catch(ArithmeticException e) {
-                throw new IllegalArgumentException("altitude gains resulted in integer overflow"); }
+                throw new ArithmeticException("exception from current altitude "
+                        + currAlt + " + gain " + gain + " due to integer overflow at index " + i); }
 
             maxAlt = Math.max(currAlt, maxAlt);
         }
@@ -27,12 +30,12 @@ public class HighestAltitude {
         System.out.println("1732. Find the Highest Altitude");
 
         // example 1
-        int[] a1 = {-5,1,5,0,-7};
+        int[] a1 = {-5, 1, 5, 0, -7};
         System.out.println("\nAltitude Gains: " + Arrays.toString(a1));
         System.out.println("Highest Altitude: " + largestAltitude(a1));
 
         // example 2
-        int[] a2 = {-4,-3,-2,-1,4,3,2};
+        int[] a2 = {-4, -3, -2, -1, 4, 3, 2};
         System.out.println("\nAltitude Gains: " + Arrays.toString(a2));
         System.out.println("Highest Altitude: " + largestAltitude(a2));
     }
