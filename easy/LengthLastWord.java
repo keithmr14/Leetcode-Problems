a9e780e913b0a4b493420b4388db75d6d901111b
Keith Marlou Aldueza <keithmr14@gmail.com>
2026-08-26 19:24:36 +0800

    Created LengthLastWord.java

---
+++ easy/LengthLastWord.java
@@ -0,0 +1,35 @@
+package easy;
+
+public class LengthLastWord {
+
+    public static int lengthOfLastWord(String s) {
+
+        int wordLength = 0;
+        // start checking for word at the end of string
+        for(int i = s.length() - 1; i >= 0; i--) {
+
+            if(s.charAt(i) == ' ') {
+                // if a word has already formed
+                if(wordLength > 0) return wordLength;
+            }
+            else wordLength++; // detecting a letter
+        }
+
+        return wordLength; // if no space detected
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("58. Length of Last Word");
+
+        // example 1
+        String s1 = "Hello World";
+        System.out.println("\nString: \"" + s1 + "\"");
+        System.out.println("Length: " + lengthOfLastWord(s1));
+
+        // example 2
+        String s2 = "   fly me   to   the moon  ";
+        System.out.println("\nString: \"" + s2 + "\"");
+        System.out.println("Length: " + lengthOfLastWord(s2));
+    }
+}
